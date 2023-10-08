8b092f79f2d3f1e7095bb84ce5ffcacd741e7946
Amarnath Tripathy <114403813+amarrr11@users.noreply.github.com>
2023-10-08 23:24:44 +0530

    Add files via upload

---
+++ lec1.cpp
@@ -0,0 +1,35 @@
+//1
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter tha value of a: "<<endl;
+    cin>>a;
+    cout<<"Enter the value of b :"<<endl;
+    cin >>b;
+    cout<<"The sum of both the numbers are : "<<a+b<<endl;
+    return 0;
+}
+
+//2
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter tha value of a: "<<endl;
+    cin>>a;
+    cout<<"Enter the value of b :"<<endl;
+    cin >>b;
+    if (a>b){
+        cout<<"the sum of the numbers are "<<a+b<<endl;
+    }
+    else if(a<b){
+        cout<<"The subtraction of the number is "<<a-b<<endl;
+    }
+    else if(a==b){
+        cout<<"Both the integers are equal"<<endl;
+    }
+    return 0;
+}
+//Amarnath Tripathy
